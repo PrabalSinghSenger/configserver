3346c262e4cb3ac5c9d6218f7a52050c7acb960e
Prabal Pratap Singh <ppsingh.singh2@gmail.com>
2023-09-26 15:36:00 +0530

    add changes

--- src/main/java/com/cloud/config/server/configserver/ConfigserverApplication.java
+++ src/main/java/com/cloud/config/server/configserver/ConfigserverApplication.java
@@ -2,8 +2,12 @@ package com.cloud.config.server.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+
+@EnableConfigServer
 @SpringBootApplication
+
 public class ConfigserverApplication {
 
 	public static void main(String[] args) {
